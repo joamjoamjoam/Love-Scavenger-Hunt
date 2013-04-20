a8fb7ae47554b724921a62bd6f79d11cfe36e6e3
joamjoamjoam <trentcalllan@yahoo.com>
2013-04-19 20:01:27 -0700

    second commit

---
+++ gdsgd.h
@@ -0,0 +1,13 @@
+//
+//  gdsgd.h
+//  Love Scavenger Hunt
+//
+//  Created by Trent Callan on 4/19/13.
+//  Copyright (c) 2013 Trent Callan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gdsgd : UITableViewController
+
+@end
---
+++ gdsgd.m
@@ -0,0 +1,13 @@
+//
+//  gdsgd.m
+//  Love Scavenger Hunt
+//
+//  Created by Trent Callan on 4/19/13.
+//  Copyright (c) 2013 Trent Callan. All rights reserved.
+//
+
+#import "gdsgd.h"
+
+@implementation gdsgd
+
+@end
